0be7e049fa81d91d7e96e25a03f813c2f738bc00
fabioilmagnifico <amogusfabio@gmail.com>
2022-11-05 18:11:58 +0100

    Beeendete version der gui:white_check_mark:

--- gui.py
+++ gui.py
@@ -1,5 +1,6 @@
 from tkinter import *
-
+from tkinter import filedialog
+from tkinter import messagebox
 #Definiere variebeln
 BreiteCanvas=900
 HoeheCanvas=590
@@ -7,7 +8,8 @@ BreiteFenster=1000
 HoeheFenster=600
 #Funktion Knopf/Eingabe
 def Knopfdruck():
-    Grafik.create_rectangle(20, 20 ,BreiteCanvas-20, HoeheCanvas-40)
+    Formel2 = eingabe.get()
+    FunktionFrame(OptionFrame, Funktion(Formel2, farbe="Blue"))
 
 class Funktion:
     def __init__(self, term, farbe="black"):
@@ -15,14 +17,45 @@ class Funktion:
         self.term=term
     def eval (self, x):
         return eval(self.term, locals={"x":x})
-         
+
+class FunktionFrame:
+    def __init__(self, parent, Funktion, farbe="Green"):
+        self.parent = parent
+        self.Funktion = Funktion
+        self.FunktionKasten= Frame(parent, bg=farbe,width=parent.winfo_width(), height=90, relief=SOLID, borderwidth=3)
+        self.FunktionKasten.pack()
+        self.FunktionKasten.pack_propagate(0)
+        self.DeleteButton = Button(self.FunktionKasten, text="X", command=self.delete)
+        self.DeleteButton.pack(anchor=NE)
+        self.Formel = Label(self.FunktionKasten, text=self.Funktion.term, foreground=self.Funktion.farbe)
+        self.Formel.pack(anchor=NW)
+        self.EditButton = Button(self.FunktionKasten, text="Bearbeite", command=self.AendereFunktion)
+        self.EditButton.pack(side=RIGHT)
+
+       
+    def delete(self):
+        if messagebox.askokcancel("Löschen", "Wirklich Löschen?"):
+            self.FunktionKasten.pack_forget()
+            self.FunktionKasten.destroy()
+    def AendereFunktion(self):
+        self.AendereFenster= Tk() 
+        self.Eingabe2 = Entry(self.AendereFenster)
+        self.Eingabe2.pack()
+        self.ButtonOk = Button(self.AendereFenster, text="Ok", command=self.destroy)
+        self.ButtonOk.pack()
+        self.AendereFenster.mainloop()
+    def destroy(self):
+        self.Aenderung=self.Eingabe2.get()
+        str(self.Aenderung)
+        self.Formel.config(text=self.Aenderung)
+        self.AendereFenster.destroy()
 #Initialisiere fenster/canvas/Knopf
 Fenster = Tk()
 Fenster.title("Mathebuchvisualisierer")
 Fenster.config(width=HoeheFenster, height=BreiteFenster)
 
 #Frames
-OptionFrame = Frame(Fenster, width=100, bg="Blue", height=HoeheFenster)
+OptionFrame = Frame(Fenster, width=200, bg="Blue", height=HoeheFenster)
 OptionFrame.pack(side=LEFT, fill=Y, expand=False)
 CanvasFrame = Frame(Fenster, width=BreiteFenster-100, bg="Red", height=HoeheFenster-40)
 CanvasFrame.pack(anchor=NE, fill=BOTH, expand=True)
@@ -40,6 +73,8 @@ eingabe = Entry(EingabeFrame, font=("Arial",16))
 eingabe.pack(side=LEFT, fill=X, expand=True)
 Knopf.pack(side=RIGHT, fill=Y)
 Grafik.pack(fill=BOTH, expand=True)
+
+
 
 #Aktualisiere fenster
 Fenster.mainloop()
