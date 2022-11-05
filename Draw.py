00d2a230faffa3f891351f7dc4edb205e24f2b0a
AlpakaMagic2022 <judith.schrammqvodafone.de>
2022-11-05 19:03:43 +0000

    Canvas class skeleton

--- Draw.py
+++ Draw.py
@@ -3,6 +3,42 @@ import numpy as np
 from tkinter  import*
 top = Tk()
 # Code to add widgets will go here...
+from PIL import Image, ImageTk, ImageDraw
+
+class Zeichenbrett:
+    def __init__(self,canvas):
+        canvas.update()
+        self.content=Image.new('RGB', (canvas.winfo_width(),canvas.winfo_height()))
+        self.draw = ImageDraw.Draw(self.content)
+        
+        self.width = canvas.winfo_width()
+        self.height = canvas.winfo_height()
+        self.canvas = canvas
+        self.scale = 20
+        
+        self.img = ImageTk.PhotoImage(self.content)
+        self.container = canvas.create_image(0, 0, image=self.img, anchor=NW)
+
+
+
+    def update(self):
+        self.img = ImageTk.PhotoImage(self.content)
+        self.canvas.itemconfig(self.container,image=self.img)
+    
+    def reset(self):
+        pass
+    
+    def karo(self):
+        pass
+
+    def coordinatesystem(self, xmax, xmin, ymax, ymin):
+        pass
+
+    
+
+    def drawFunction(self, f, start, end, step, colour):
+        pass
+
 canvas = Canvas(top,width = 1000, height = 1000, bg="#FFFFFF")
 canvas.pack()
 
@@ -73,6 +109,9 @@ Karo(canvas)
 Koordinatensystem(canvas)
 Graphzeichnen(canvas)
 Kreis(canvas)
+
+zb=Zeichenbrett(canvas)
+zb.update()
 
 top.mainloop()
 
